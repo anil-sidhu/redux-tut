0f5779868d9a4a01c4b595f88e8c36e3d05e86af
anil <webanilsidhu@gmail.com>
2020-09-05 13:30:17 +0530

    add to cart desgin done

---
+++ src/components/Home.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div>
+            <div className="add-to-cart">
+                <img src="https://static.vecteezy.com/system/resources/thumbnails/000/496/007/small/Ecommerce_998.jpg" />
+            </div>
+            <h1>Home Component</h1>
+            <div className="cart-wrapper">
+                <div className="img-wrapper item">
+                    <img src="https://www.fdfproject.com/wp-content/uploads/2018/12/iphone-png.png" />
+                </div>
+                <div className="text-wrapper item">
+                    <span>
+                        I-Phone
+                    </span>
+                    <span>
+                        Price: $1000.00
+                    </span>
+                </div>
+                <div className="btn-wrapper item">
+                    <button>Add To Cart</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Home
